7c34fb2daa462d1b32ed9fec110b374f56ebb48b
halileryildirim <83421076+halileryildirim@users.noreply.github.com>
2024-05-10 03:24:03 +0300

    Add game form midware and view

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -50,14 +50,79 @@ exports.game_detail = asyncHandler(async (req, res, next) => {
 });
 
 // Display Game create form on GET.
-exports.game_create_get = (req, res, next) => {
-  res.render("layout", { content: "game_form", title: "Create Game" });
-};
+exports.game_create_get = asyncHandler(async (req, res, next) => {
+  const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+  res.render("layout", {
+    content: "game_form",
+    title: "Create Games",
+    categories: allCategories,
+  });
+});
 
 // Display Game create form on POST.
-exports.game_create_post = asyncHandler(async (req, res, next) => {
-  res.send("TBA: Game create POST");
-});
+exports.game_create_post = [
+  // Convert the categories into an array.
+  (req, res, next) => {
+    if (!Array.isArray(req.body.category)) {
+      req.body.category =
+        typeof req.body.category === "undefined" ? [] : [req.body.category];
+    }
+    next();
+  },
+
+  // Validate and sanitize fields.
+  body("name", "Name must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("release_year", "Invalid Date")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  body("platform").escape(),
+  body("category.*").escape(),
+
+  //Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Game object with escaped and trimmed data.
+    const game = new Game({
+      name: req.body.name,
+      description: req.body.description,
+      release_year: req.body.release_year,
+      platform: req.body.platform,
+      category: req.body.category,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values / error messages.
+
+      // Get all categories for form.
+      const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+      // Mark selected categories as checked.
+      for (const category of allCategories) {
+        if (game.category.includes(category._id)) {
+          category.checked = "true";
+        }
+      }
+      res.render("layout", {
+        content: "game_form",
+        title: "Create Game",
+        categories: allCategories,
+        errors: errors.array(),
+      });
+    } else {
+      // Data from form is valid. Save Game.
+      await game.save();
+      res.redirect(game.url);
+    }
+  }),
+];
 
 // Display Game delete form on GET.
 exports.game_delete_get = asyncHandler(async (req, res, next) => {
